774d0d51717e1f13b3c2de14b9e9047721db559f
pasindusandun <sandungwp@gmail.com>
2023-11-28 17:13:26 +0530

    return change no file input

--- API_Implementation.py
+++ API_Implementation.py
@@ -44,33 +44,37 @@ def hello_world():
 
 
 @app.post("/demographicsImage")
-async def create_upload_file(file: UploadFile = File(...)):
-    file.filename = f"{uuid.uuid4()}.jpg"
-    contents = await file.read()  
+async def create_upload_file(
+    # file: UploadFile = File(...)
+    ):
+    # file.filename = f"{uuid.uuid4()}.jpg"
+    # contents = await file.read()  
 
-    # example of how you can save the file
-    with open(f"{IMAGEDIR}{file.filename}", "wb") as f:
-        f.write(contents)
+    # # example of how you can save the file
+    # with open(f"{IMAGEDIR}{file.filename}", "wb") as f:
+    #     f.write(contents)
 
-    filepath = IMAGEDIR + file.filename
-    # filepath = IMAGEDIR + "A.jpg"
+    # filepath = IMAGEDIR + file.filename
+    filepath = IMAGEDIR + "A.jpg"
 
     # df = pd.DataFrame(columns = ['path'])
     # Add records to dataframe using the .loc function
     # df.loc[0] = [filepath] 
     # Result = ModelLoading.finalImageOutput(df)
     gender = PredictionModel.predict_gender(filepath)
-    # emotion = emotionDetection.predict_emotion(filepath)
+    emotion = emotionDetection.predict_emotion(filepath)
     # age = ageDetection.predict_age()
     # res =  PredicNew.finalImageOutput()
     # Result = ModelLoading.finalImageOutput(contents)
-    # age = deepFaceFW.predict_age(filepath)
+    age = deepFaceFW.predict_age(filepath)
    
     # return age
     return {"Gender": gender, 
-            "Age": "20", 
-            "Emotion": "Normal"
-            # emotion['main_emotion']
+            # "Age": "20", 
+            "Emotion": emotion['main_emotion'],
+            "Age": age[0]['age']
+            # "Emotion":"normal"
+            
             }
     # return {"Gender": "Male", 
     #         "Age":"20", 
